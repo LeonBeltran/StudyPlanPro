29299578cb99d674aea0fa1c0f3b7f68c4312500
ecisip <emmersonisip07@gmail.com>
2024-02-22 19:09:28 +0800

    Added Barebones UI for Homepage

---
+++ StudyPlanPro/templates/index.js
@@ -0,0 +1,9 @@
+function loginPopup() {
+    const popUp = document.getElementById("popup");
+    popUp.style.visibility = "visible";
+}
+
+function exitPopup() {
+    const popUp = document.getElementById("popup");
+    popUp.style.visibility = "hidden";
+}
