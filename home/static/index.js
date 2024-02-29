800bae5f38ebf5840da46616b478a6079814393d
ecisip <emmersonisip07@gmail.com>
2024-02-29 15:38:05 +0800

    added UI handling for erroneous inputs

--- home/static/index.js
+++ home/static/index.js
@@ -1,3 +1,5 @@
+const { text } = require("stream/consumers");
+
 function loginPopup() {
     const popUp = document.getElementById("popup");
     popUp.style.visibility = "visible";
@@ -5,5 +7,35 @@ function loginPopup() {
 
 function exitPopup() {
     const popUp = document.getElementById("popup");
+    
+    const textFields = document.getElementsByClassName("loginInput");
+    const nicknameField = textFields[0];
+    const emailField = textFields[1];
+
     popUp.style.visibility = "hidden";
+    nicknameField.setAttribute("style", "");
+    nicknameField.value = "";
+    emailField.setAttribute("style", "");
+    emailField.value = "";
 }
+
+
+
+// TEMPORARY FUNCTION TO SHOW UI WHEN ERROR IN INPUTS IS ENCOUNTERED //
+
+function sendEmailPressed() {
+    const textFields = document.getElementsByClassName("loginInput");
+    const nickname = textFields[0].value.replace(/\s/g, '');
+    const email = textFields[1].value.replace(/\s/g, '');
+
+    textFields[0].setAttribute("style", "border-color: red; border-style: solid;");
+    textFields[1].setAttribute("style", "border-color: red; border-style: solid;");
+
+    if (!nickname || !email) {
+        alert("nickname and email fields can't be empty");
+    } else {
+        alert("Invalid UP email");
+    }
+}
+
+////////////////////////////////////////////////////////////////////////
