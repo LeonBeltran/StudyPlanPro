87f234b1da3af248b06170001fcc072caa51b890
caspikk <gosh124x@gmail.com>
2024-03-18 09:45:21 +0800

    Fixing database
    Had bugs, but cleared it out.

--- home/models.py
+++ home/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
 class Course(models.Model):
-    courseCode = models.CharField(max_length=20, blank=True)
+    courseCode = models.CharField(max_length=30)
     courseTitle = models.CharField(max_length=100)
-    shortDescription = models.CharField(max_length=300)
-    courseProfessors = models.CharField(max_length=300)
+    shortDescription = models.CharField(max_length=300, blank=True)
+    courseProfessors = models.CharField(max_length=300, blank=True)
     coursePolicies = models.CharField(max_length=300, blank=True)
-    nominalCourseDifficultyReview = models.IntegerField()
-    courseDemand = models.IntegerField(blank=True)
-    passingRate = models.FloatField(blank=True)
+    nominalCourseDifficultyReview = models.IntegerField(default=3, validators=[MinValueValidator(1), MaxValueValidator(5)])
+    courseDemand = models.IntegerField(null=True, blank=True)
+    passingRate = models.FloatField(null=True, blank=True)
 
 class CourseReview(models.Model):
     course = models.ForeignKey(Course, on_delete = models.CASCADE)
