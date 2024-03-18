9e7b3f5cb5d24351b70ab2ca291305eea10ae480
caspikk <gosh124x@gmail.com>
2024-03-18 11:09:42 +0800

    Created initialized data [w/ BUG]
    BUG: prerequisite field appears on both ends, thus both instances will have share each other as prerequisites

--- home/admin.py
+++ home/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from .models import Course
 from .models import CourseReview
-from .models import StudentPassedCourse
-from .models import StudentFailedCourse
 from .models import Student
 
 # Register your models here.
 admin.site.register(Course)
 admin.site.register(CourseReview)
-admin.site.register(StudentPassedCourse)
-admin.site.register(StudentFailedCourse)
 admin.site.register(Student)
--- home/models.py
+++ home/models.py
@@ -5,26 +5,25 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Course(models.Model):
     courseCode = models.CharField(max_length=30)
     courseTitle = models.CharField(max_length=100)
-    shortDescription = models.CharField(max_length=300, blank=True)
-    courseProfessors = models.CharField(max_length=300, blank=True)
-    coursePolicies = models.CharField(max_length=300, blank=True)
+    shortDescription = models.CharField(max_length=500, blank=True)
+    coursePrereq = models.ManyToManyField('self', blank=True)
+    courseCoreq = models.ManyToManyField('self', blank=True)
     nominalCourseDifficultyReview = models.IntegerField(default=3, validators=[MinValueValidator(1), MaxValueValidator(5)])
-    courseDemand = models.IntegerField(null=True, blank=True)
-    passingRate = models.FloatField(null=True, blank=True)
+    courseDemand = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.courseCode
 
 class CourseReview(models.Model):
     course = models.ForeignKey(Course, on_delete = models.CASCADE)
     courseReview = models.CharField(max_length=500, blank=True)
 
-class StudentPassedCourse(models.Model):
-    course = models.OneToOneField(Course, on_delete = models.CASCADE)
-    
-class StudentFailedCourse(models.Model):
-    course = models.OneToOneField(Course, on_delete = models.CASCADE)
-
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     systemReview = models.CharField(max_length=500, blank=True)
-    passedCourses = models.ManyToManyField(StudentPassedCourse, blank=True)
-    failedCourses = models.ManyToManyField(StudentFailedCourse, blank=True)
+    passedCourses = models.ManyToManyField(Course, blank=True, related_name="PassedCourse")
+    failedCourses = models.ManyToManyField(Course, blank=True, related_name="FailedCourse")
+
+    def __str__(self):
+        return self.email
