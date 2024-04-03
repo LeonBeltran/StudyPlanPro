5631593a4f7249d9f2ea959dadeb0723fba1f17b
LeonBeltran <enbeltranii@gmail.com>
2024-04-03 17:06:21 +0800

    added user signup

--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
+from .forms import StudentForm
 
 # Create your views here.
 def view_home(request):
      return render(request, 'homepage.html')
+
+def view_join(request):
+     if request.method == "POST":
+          form = StudentForm(request.POST or None)
+          if form.is_valid():
+               form.save()
+          return redirect('/home/')
+     else:
+          return render(request, 'joinpage.html')
 
 def view_logout(request):
      logout(request)
