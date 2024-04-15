d8420d31425a360f0045f5e878ac7ce1f0103b3a
caspikk <gosh124x@gmail.com>
2024-04-15 17:20:12 +0800

    tried demand table functionality

--- demand/views.py
+++ demand/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from home.models import Course
 
 # Create your views here.
 def view_demand(request):
-     return render(request, 'demandpage.html')
+     course_list = Course.objects.all().filter(courseCode__startswith = "CS")           # FOR DATABASE FUNCTIONALITY
+     return render(request, 'demandpage.html', {'all_courses': course_list})
