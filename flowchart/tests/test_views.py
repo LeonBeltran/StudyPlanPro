9ec725ec22e1306dd4d5ae65a8a275f90dc3deea
omeeeeeeee <naamparo@up.edu.ph>
2024-04-18 22:50:53 +0800

    Added unit tests in flowchart/tests/test_views.py

--- flowchart/tests/test_views.py
+++ flowchart/tests/test_views.py
@@ -7,6 +7,8 @@ class TestViewsFlowchart(TestCase):
     def setUp(self):
         self.client = Client()
         self.flowchart_url = reverse("flowchart")
+        self.othercourses_url = reverse("othercourses")
+        self.recommendations_url = reverse("recommendations")
 
     def test_flowchart_GET(self):
         response = self.client.get(self.flowchart_url)
@@ -14,3 +16,17 @@ class TestViewsFlowchart(TestCase):
         
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "flowchartpage.html")
+
+    def test_othercourses_GET(self):
+        response = self.client.get(self.othercourses_url)
+        print("test_othercourses_GET", str(response.status_code))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "othercoursespage.html")
+
+    def test_recommendations_GET(self):
+        response = self.client.get(self.recommendations_url)
+        print("test_recommendations_GET", str(response.status_code))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "recommendationspage.html")
