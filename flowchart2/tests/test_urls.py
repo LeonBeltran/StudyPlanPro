4fd28f0cda17c5db18edf43e9a7f7203ec16cc4e
omeeeeeeee <naamparo@up.edu.ph>
2024-04-30 12:24:10 +0800

    Fixed test cases in flowchart 2 test_urls.py

--- flowchart2/tests/test_urls.py
+++ flowchart2/tests/test_urls.py
@@ -1,19 +1,19 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from flowchart.views import view_flowchart, view_othercourses, view_recommendations
+from flowchart2.views import view_flowchart, view_othercourses, view_recommendations
 
 class TestUrls(SimpleTestCase):
     def test_flowchart(self):
-        url = reverse('flowchart')
+        url = reverse('flowchart2')
         print(resolve(url))
         self.assertEqual(resolve(url).func, view_flowchart)
         
     def test_othercourses(self):
-        url = reverse('othercourses')
+        url = reverse('othercourses2')
         print(resolve(url))
         self.assertEqual(resolve(url).func, view_othercourses)
         
     def test_recommendations(self):
-        url = reverse('recommendations')
+        url = reverse('recommendations2')
         print(resolve(url))
         self.assertEqual(resolve(url).func, view_recommendations)
