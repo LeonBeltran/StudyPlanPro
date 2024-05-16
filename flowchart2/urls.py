73f7f932c1769be3d0769d9e6330aed7d2d2d366
caspikk <gosh124x@gmail.com>
2024-05-16 22:02:56 +0800

    fixed flowchart2

--- flowchart2/urls.py
+++ flowchart2/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.view_flowchart, name="flowchart2"),
+    path('course_description/', views.course_description, name='course_description'),
+    path('add_course_review/', views.add_course_review, name='add_course_review2'),
     path('othercourses/', views.view_othercourses, name="othercourses2"),
     path('recommendations/', views.view_recommendations, name="recommendations2")
 ]
