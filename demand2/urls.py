c4f674295832d40a0b5404324a11bfc8e19f4860
caspikk <gosh124x@gmail.com>
2024-05-16 17:08:41 +0800

    fixed demandtable2

--- demand2/urls.py
+++ demand2/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_demand, name="demand2")
+    path('', views.view_demand, name="demand2"),
+    path('course_description/', views.course_description, name='course_description')
 ]
--- demand2/views.py
+++ demand2/views.py
@@ -1,19 +1,57 @@
 from django.shortcuts import render
-# from home.models import Course
-# from .filters import CourseFilter
-
-# Create your views here.
-def view_demand(request):
-     return render(request, 'demand2page.html')
+from django.http import JsonResponse
+from home.models import Course, CourseReview
+from .filters import CourseFilter
 
 ### FROM ORIGINAL DEMAND TABLE ###
-# def view_demand(request):
-#      course_list = Course.objects.filter(courseCode__startswith = "CS")           # FOR DATABASE FUNCTIONALITY
-#      searched_course = CourseFilter(request.GET, queryset=course_list)
-#      course_list = searched_course.qs
-#      context = {
-#           'course_list': course_list,
-#           'searched_course': searched_course
-#      }
-#      return render(request, 'demandpage.html', context)
-### ### ###
+def view_demand(request):
+     if request.method == 'POST':
+          course_review = request.POST['review_input']
+          course = request.POST.get('review_course')
+          course = Course.objects.get(courseCode=course)
+          new_review = CourseReview(course=course, courseReview=course_review)
+          new_review.save()
+
+     course_list = Course.objects.filter(courseCode__startswith = "EEE") | Course.objects.filter(courseCode__startswith = 'CoE')           # FOR DATABASE FUNCTIONALITY
+     searched_course = CourseFilter(request.GET, queryset=course_list)
+     course_list = searched_course.qs
+     context = {
+          'course_list': course_list,
+          'searched_course': searched_course
+     }
+     return render(request, 'demand2page.html', context)
+
+def course_description(request):
+     if request.method == 'GET':
+          data = request.GET.get('code')
+          course = Course.objects.get(courseCode=data)
+          prereqs = []
+          coreqs = []
+          neededfor = []
+          reviews = []
+          for prereq in course.coursePrereq.all():
+               prereqs.append(prereq.courseCode)
+          for coreq in course.courseCoreq.all():
+               coreqs.append(coreq.courseCode)
+          for courses in Course.objects.all():
+               for courses_prereq in courses.coursePrereq.all():
+                    if course == courses_prereq:
+                         neededfor.append(courses.courseCode)
+          for review in CourseReview.objects.all():
+               if course == review.course:
+                    reviews.append(review.courseReview)
+          data = {
+               "courseCode": course.courseCode,
+               "courseTitle": course.courseTitle,
+               "shortDescription": course.shortDescription,
+               "coursePrereq": prereqs,
+               "courseCoreq": coreqs,
+               "neededFor": neededfor,
+               "courseReview": reviews
+          }
+          print(data)
+          return JsonResponse(data)
+     elif Course.DoesNotExist:
+          return JsonResponse({"error": "Course does not exist"}, status=404)
+     else:     
+          return JsonResponse({"error": str(Exception)}, status=500)
